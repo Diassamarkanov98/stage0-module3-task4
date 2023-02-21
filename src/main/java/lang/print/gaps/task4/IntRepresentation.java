9f951d0e2429ea3aad1d15ff77a325b268886426
Dias <diassamarkanov108@gmail.com>
2023-02-21 18:26:40 +0600

    init: start module 3 task 4

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,17 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int x = 99;
+        int y = 104;
+        int z = 97;
+        int g = 114;
+        char c = (char) x;
+        char h = (char) y;
+        char a = (char) z;
+        char r = (char) g;
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
